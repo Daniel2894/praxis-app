ea6a00be0d86f40b1e820f9a420cd906539e67ec
Daniel Restrepo Mejia <drestrepom@psl.com.co>
2021-02-25 10:15:23 -0500

    initial commit

---
+++ src/main/java/com/example/praxisappbe/PraxisAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.praxisappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraxisAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraxisAppBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/praxisappbe/PraxisAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praxisappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraxisAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
